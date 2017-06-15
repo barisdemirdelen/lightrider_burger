eb786c596c8556cfd904f9b414ec96e02e386161
Baris Demirdelen <barisdemirdelen@gmail.com>
2017-06-15 10:58:17 +0200

    Fix middle block when players adjacent

--- src/bot/board.py
+++ src/bot/board.py
@@ -86,13 +86,10 @@ class Board(object):
             result.append((row, col - 1))
         return result
 
-    def get_adjacent_with_players(self, row, col, player_id, player_adjacent_neighbour):
+    def get_adjacent_with_players(self, row, col, player_id):
         result = []
         for (o_row, o_col), _ in DIRS:
             t_row, t_col = o_row + row, o_col + col
-            if not player_adjacent_neighbour:
-                if self.in_bounds(t_row, t_col) and self.cell[row][col] + self.cell[t_row][t_col] == 1:
-                    continue
             if self.is_legal_with_players(t_row, t_col, player_id):
                 result.append((t_row, t_col))
         return result
@@ -128,7 +125,7 @@ class Board(object):
         sys.stderr.flush()
 
     def total_area(self, coord, player_id=0):
-        childs = self.get_adjacent(*coord)
+        childs = self.get_adjacent(coord[0], coord[1], player_id)
         best_area = 0
         for child in childs:
             area = set()
@@ -186,30 +183,17 @@ class Board(object):
                                                                                                  self.players[1].col
         return field
 
-    # def is_players_separated(self):
-    #     area = set()
-    #     queue = set()
-    #     queue.add(self.players[0].coord)
-    #     while len(queue) > 0:
-    #         current = queue.pop()
-    #         if current == self.players[1].coord:
-    #             return False
-    #         area.add(current)
-    #         current_adjacent = self.get_adjacent_with_players(*current)
-    #         for adjacent in current_adjacent:
-    #             if adjacent not in area and adjacent not in queue:
-    #                 queue.add(adjacent)
-    #     return True
-
     def is_players_separated(self):
         return self.get_player_true_distance() == float("inf")
 
-    def a_star_player_to_enemy(self, player_id, player_adjacent_neighbour=True):
+    def a_star_player_to_enemy(self, player_id, prevent_passing=None):
         return self.a_star(self.players[player_id].coord, self.players[player_id ^ 1].coord, player_id,
-                           player_adjacent_neighbour)
+                           prevent_passing)
 
-    def a_star(self, start, goal, player_id=0, player_adjacent_neighbour=True):
-        field_hash = hash(str([start, goal, player_id, player_adjacent_neighbour] + self.cell))
+    def a_star(self, start, goal, player_id=0, prevent_passing=None):
+        if prevent_passing is None:
+            prevent_passing = defaultdict(set)
+        field_hash = hash(str([start, goal, player_id, prevent_passing] + self.cell))
         if field_hash in self.distance_cache:
             return self.distance_cache[field_hash]
         closed_set = set()
@@ -231,9 +215,9 @@ class Board(object):
             open_set.remove(current)
             closed_set.add(current)
 
-            neighbours = self.get_adjacent_with_players(current[0], current[1], player_id, player_adjacent_neighbour)
+            neighbours = self.get_adjacent_with_players(current[0], current[1], player_id)
             for neighbour in neighbours:
-                if neighbour in closed_set:
+                if neighbour in closed_set or neighbour in prevent_passing[current]:
                     continue
 
                 open_set.add(neighbour)
@@ -257,34 +241,51 @@ class Board(object):
         return list(reversed(total_path[:-1]))
 
     def block_middle(self):
-        field = self.block_middle_for_player(0)
-        field = field.block_middle_for_player(1)
+        field, prevent_p1 = self.block_middle_for_player(0)
+        field, prevent_p2 = field.block_middle_for_player(1)
+
+        for prevent_from in prevent_p1.keys():
+            for prevent_to in prevent_p1[prevent_from]:
+                field.cell[prevent_from[0]][prevent_from[1]] = 4
+                field.cell[prevent_to[0]][prevent_to[1]] = 5
+
+        for prevent_from in prevent_p2.keys():
+            for prevent_to in prevent_p2[prevent_from]:
+                field.cell[prevent_from[0]][prevent_from[1]] = 5
+                field.cell[prevent_to[0]][prevent_to[1]] = 4
+
+        field.cell[field.players[0].row][field.players[0].col] = 0
+        field.cell[field.players[1].row][field.players[1].col] = 1
+
         return field
 
     def block_middle_for_player(self, player_id):
         field = self.get_copy()
         first = True
         path = None
+        prevent = defaultdict(set)
+        player = field.players[player_id]
         while first or path is not None:
             first = False
-            path = field.a_star_player_to_enemy(player_id, player_adjacent_neighbour=False)
+            path = field.a_star_player_to_enemy(player_id, prevent_passing=prevent)
             if path is None:
                 break
             path = path[:-1]
             if len(path) == 0:
                 # TODO buraya bişey yap
-                sys.stderr.write('block middle path len 0?\n')
-                sys.stderr.flush()
-                break
-            if len(path) % 2 == 0:
+                # sys.stderr.write('block middle path len 0?\n')
+                # sys.stderr.flush()
+                prevent[player.coord].add(field.players[player_id ^ 1].coord)
+            elif len(path) % 2 == 0:
                 c1 = path[len(path) // 2 - 1]
                 c2 = path[len(path) // 2]
-                field.cell[c1[0]][c1[1]] = 4
-                field.cell[c2[0]][c2[1]] = 5
+                prevent[c1].add(c2)
+                # field.cell[c1[0]][c1[1]] = 4
+                # field.cell[c2[0]][c2[1]] = 5
             else:
                 c = path[(len(path) - 1) // 2]
                 field.cell[c[0]][c[1]] = BLOCKED
-        return field
+        return field, prevent
 
     def set_cell(self, cell):
         self.cell = cell
